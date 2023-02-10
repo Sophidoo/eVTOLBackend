596be72d1bb769c40e9bea0fbde767badf6ad066
Sophia Okosodo <sophieokosodo@gmail.com>
2023-02-10 21:28:56 +0100

    minor cchanges

--- model/EvtolModel.js
+++ model/EvtolModel.js
@@ -11,7 +11,7 @@ const evtolSchema = new mongoose.Schema({
         required: [true, "Please choose a model for the evtol"]
     },
     weight: {
-        type: mongoose.Types.Decimal128,
+        type: Number,
         required: [true, "Please enter the weight limit"]
     },
     batteryCapacity: {
