2c02097a18ddbb78d2bb20afbd1f889bade3b8ef
Sophia Okosodo <sophieokosodo@gmail.com>
2023-02-11 10:03:58 +0100

    mistake

--- routes/EvtolRoutes.js
+++ routes/EvtolRoutes.js
@@ -14,7 +14,7 @@ evtolRoutes.put("/user/:id", isLogin, currentUserOfEvtol)
 evtolRoutes.put("/edit/:id", evtolEditController)
 
 // get evtol b y serial
-evtolRoutes.put("/:serialno", getEvtolBySerial)
+evtolRoutes.post("/:serialno", getEvtolBySerial)
 
 // change state
 evtolRoutes.put("/changestate/:id", changeStateController)
