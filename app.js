0327ae165629b7164128b4d2f4b65308b8799c0f
Sophia Okosodo <sophieokosodo@gmail.com>
2023-02-11 07:01:44 +0100

    ccors

--- app.js
+++ app.js
@@ -33,13 +33,27 @@ app.use(
   })
 )
 
-app.options('*', cors())
 app.use(express.json());
 
 app.use("/api/v1/users", userRoutes);
 
 app.use("/api/v1/medications", medicationRoutes);
 
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Credentials","true");
+  next();
+});
+app.options('*', cors())
 app.use("/api/v1/evtol", evtolRoutes)
 
 
